2f8c9ddb158687e2edfcb2b27d323e24c73ce31c
Matt Moore <matt.m.moore13@outlook.com>
2021-02-18 01:10:53 -0500

    Added the changePassword api request. Needed to refresh the Token request method- how do we incorporate this with interpolation?

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -18,7 +18,20 @@ const signIn = function (data) {
   })
 }
 
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+    // data: data
+  })
+}
+
 module.exports = {
   signUp,
-  signIn
+  signIn,
+  changePassword
 }
