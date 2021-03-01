76c733c40826bed29721b0473688dccbd47f3059
Matt Moore <>
2021-03-01 16:58:10 -0500

    Provided some content to the How To modal.

--- assets/scripts/templates/staticmodals.js
+++ assets/scripts/templates/staticmodals.js
@@ -22,22 +22,34 @@ const messagesHtml = `
 
 const howToHtml =
 `
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">
-          How To
-        </h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">
-              &times;
-          </span>
-        </button>
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title">
+        WaXOn,waXOff
+      </h5>
+      <button type="button" class="close" data-dismiss="modal">
+        &times;
+      </button>
       </div>
       <div class="modal-body">
         <p>
-          This is where you learn how to play Tic Tac Toe
+          Tic Tac Toe is an ancient training technique used to maintain the balance between the Xs and the Os
         </p>
+        <p>
+          This digital forray is just the next evolution of the battle; who will win?
+        </p>
+        <p>
+          There are two arenas in which you may spar:
+        </p>
+        <ul>
+          <li>
+            Quick Game
+          </li>
+          <li>
+            New Game
+          </li>
+        </ul>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">
